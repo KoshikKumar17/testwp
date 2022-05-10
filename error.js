6e5034393e5b7ba7623d767fee114f97eca9365b
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-05-10 10:27:50 +0530

    Error JS

---
+++ error.js
@@ -0,0 +1,18 @@
+// Inspiration from FayasNoushad //
+let remainingSeconds = 5;
+const countDown = document.getElementById("countdown");
+if (countDown)
+    countDown.innerHTML = remainingSeconds;
+
+const intervalId = setInterval(() => {
+    remainingSeconds--;
+
+    if (remainingSeconds === 0) {
+// Redirecting to Home Page 🤦  🙂 //
+        window.location.href = "https://KoshikKumar17.github.io";
+        window.clearInterval(intervalId);
+    } else if (countDown) {
+        countDown.innerHTML = remainingSeconds;
+    }
+}, 1000);
+// DONE ✅ FINALLY !! 
