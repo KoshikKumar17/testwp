ad7bd5a58f6fea882b9ac47e2317d6f7b0fd97a1
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-07-09 11:53:56 +0530

    Typed

---
+++ typed.js
@@ -0,0 +1,17 @@
+////////////------[ ✨ ❤️ 🇮🇳 Koshik 🇮🇳 ❤️ ✨ ]-------////////
+//-->
+// Typing text animation script (used in Pre Loader) //
+//  
+// -->
+//
+// From:- https://github.com/mattboldt/typed.js
+
+var typed = new Typed(".loading", {
+    strings: ["Please Wait...."],
+    typeSpeed: 90,
+    backSpeed: 0,
+    loop: true
+});
+
+//-->
+////////////------[ ✨ ❤️ 🇮🇳 Koshik 🇮🇳 ❤️ ✨ ]-------////////
