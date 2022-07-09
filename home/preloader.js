8c604d569ce9fe6049aa0fedcdc05d1105b95d4a
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-07-09 12:25:10 +0530

    Preloader JS

---
+++ home/preloader.js
@@ -0,0 +1,14 @@
+////////////------[ ✨ ❤️ 🇮🇳 Koshik 🇮🇳 ❤️ ✨ ]-------////////
+//-->
+//---> Fade out
+//
+$(document).ready(function() {
+    $('.preloader').animate({
+        'opacity': '0',
+    }, 4600);
+    setTimeout(function() {
+        $('.preloader').remove();
+    }, 2800);
+});
+//-->
+////////////------[ ✨ ❤️ 🇮🇳 Koshik 🇮🇳 ❤️ ✨ ]-------////////
