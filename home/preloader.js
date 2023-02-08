2eb27a5bc3ea71f6b518d6f8cbbf62cdcb5cf15d
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2023-02-08 10:40:45 +0530

    No need..!!

--- home/preloader.js
+++ home/preloader.js
@@ -2,13 +2,13 @@
 //-->
 //---> Fade out
 //
-$(document).ready(function() {
-    $('.preloader').animate({
-        'opacity': '0',
-    }, 4600);
-    setTimeout(function() {
-        $('.preloader').remove();
-    }, 2800);
-});
+//$(document).ready(function() {
+//    $('.preloader').animate({
+//        'opacity': '0',
+//    }, 4600);
+//    setTimeout(function() {
+//        $('.preloader').remove();
+//    }, 2800);
+//});
 //-->
 ////////////------[ ✨ ❤️ 🇮🇳 Koshik 🇮🇳 ❤️ ✨ ]-------////////
